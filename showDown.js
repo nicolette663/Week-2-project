a73131a43444cd542adc76a89aa4873d28be9899
Tinesha Conway <tineshaconway@gmail.com>
2020-10-04 21:02:08 -0400

    redid the entire coding process

--- showDown.js
+++ showDown.js
@@ -3,48 +3,67 @@ function fight() {
 	let hero = prompt("What is the name of your hero?");
 	let monsterHealth = 50; 
   	let heroHealth = 50;
-  	let playerLife = (50 - x);
-  	let x = prompt("points away from loser");
-  	let lifeLeft = (x - y);
-  	let  y = prompt("points left of loser");
+  	
+
+  	while (heroHealth > 0 && monsterHealth  > 0) {
+  		
+  		monsterDice = Math.floor(Math.random() * 20);
+      
+      heroDice = Math.floor(Math.random() * 20);
+
+  	  
+  	 if (heroDice == 1 || heroDice == 2) {
+  	 	//hero stabs for 9 point damage
+  	 	//alert new health
+		heroHealth = heroHealth - 5;
+  	 	alert(`uh-oh! ${hero} stabs self!`);
+
+  	}  	else if (heroDice >= 3 || heroDice <= 10) {
+  		alert(`${hero} misses`);
+
+  	}
+	  	else if (heroDice >= 11 || heroDice <= 18) {
+  		alert(`${hero} strikes ${monster}`);
+  		villainHealth = villainHealth - 10;
+  		
+
+  }		else if (heroDice >= 19)  {
+  		alert(`Critical hit ${hero} does double damage!`);
+  		villainHealth = villainHealth - 20;
+  		}
 
 
-  	monsterDice = Math.floor((Math.random() * 20);
-  	heroDice = Math.floor((Math.random() * 20);
+if (monsterDice == 1 || monsterDice == 2) {
+  	 	//hero stabs for 9 point damage
+  	 	//alert new health
+		monsterHealth = monsterHealth - 5;
+  	 	alert(`uh-oh! ${monster} stabs self!`);
 
-  	 if attackRoll(hero, monster) = 1 || 2 {
-  		result(stab yourself);
-  		alert(`${playerLife} take 5 points away from loser`)
-  		lifeLeft();
-}
-  	else if attackRoll() = 3 && 10; {
-  		result(lose a turn);
-  		alert(`${playerLife} take 0 points away from loser`);
-  		lifeLeft();
-}
-  	else if attackRoll() = 11 && 19;{
-  		result(hit the other);
-  		alert(`${playerLife} take 10 points away from loser`);
-  		lifeLeft();
+  	}  	else if (monsterDice >= 3 || monsterDice <= 10) {
+  		alert(`${monster} misses`);
 
-  }	else attackRoll() = 19 || 20 {
-  		result(critical hit);
-  		alert(`${playerLife} take 20 points away from loser`);
-  		lifeLeft();
-	}
+  	}
+	  	else if (monsterDice >= 11 || monsterDice <= 18) {
+  		alert(`${monster} strikes ${hero}`);
+  		villainHealth = villainHealth - 10;
+  		
+
+  }		else if (monsterDice >= 19)  {
+  		alert(`Critical hit ${hero} does double damage!`);
+  		villainHealth = villainHealth - 20;
+  		}
+
 //Fighter's health is now zero.
-if (this.lifeLeft == 0) {
-	ctx.heroHealth(this.playerLife - this.lifeLeft);
- { else }
- //TC: do this until it reaches 0--can't figure out what to do next.  
- }
+ alert(`${hero} has ${heroHealth} hitpoints left, ${monster} has ${villainHealth} left.`);
+	}
+}
 	
-
 //Find a way to let player know who won AND who lost by name.
 
 //Add a line if they both kill each other.
 
 // Below your newly defined function, invoke `fight()`.
+fight();
 
 //Go to the browser and run the file you just created.
 
@@ -65,4 +84,40 @@ if (this.lifeLeft == 0) {
 
 
 
+  while (heroHealth >=0 && villainHealth >= 0) {
+    let heroDice = Math.floor(Math.random()*20)
+    let monsterDice = Math.floor(Math.random()*20)
+
+    if (heroDice == 1 || heroDice == 2) {
+      // hero stabs himself for 5 damage
+      // alert the new heath
+      heroHealth = heroHealth - 5;
+      alert(`Critical failure! ${hero} accidentally stabs self!`);
+    } else if (heroDice >= 3 || heroDice <=10) {
+      alert(`${hero} misses`);
+    } else if (heroDice >= 11 || heroDice <= 18) {
+      alert(`${hero} strikes ${monster}`);
+      villainHealth = villainHealth - 10;
+    } else if (heroDice >= 19) {
+      alert(`Critical hit! ${hero} does double damage!`);
+      villainHealth = villainHealth - 20;
+    }
+    if (monsterDice == 1 || monsterDice == 2) {
+      // hero stabs himself for 5 damage
+      // alert the new heath
+      villainHealth = villainHealth - 5;
+      alert(`Critical failure! ${monster} accidentally stabs self!`);
+    } else if (monsterDice >= 3 || monsterDice <=10) {
+      alert(`${monster} misses`);
+    } else if (monsterDice >= 11 || monsterDice <= 18) {
+      alert(`${monster} strikes ${hero}`);
+      villainHealth = villainHealth - 10;
+    } else if (heroDice >= 19) {
+      alert(`Critical hit! ${hero} does double damage!`);
+      villainHealth = villainHealth - 20;
+    }
+    
+    alert(`${hero} has ${heroHealth} hitpoints left, ${monster} has ${villainHealth} left.`)
+  }
+}
 
