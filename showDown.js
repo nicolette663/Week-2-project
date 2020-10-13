682e6e5c13ef8ad0843de6e7d750968b0020e6e4
Tinesha N. Conway <66802899+nicolette663@users.noreply.github.com>
2020-10-12 22:30:32 -0400

    Finally finished all of showDown.js
    Redid and finished  what I did for showdown.

--- showDown.js
+++ showDown.js
@@ -2,122 +2,77 @@ function fight() {
 	let monster = prompt("What is the name of your Monster?");
 	let hero = prompt("What is the name of your hero?");
 	let monsterHealth = 50; 
-  	let heroHealth = 50;
-  	
+  let heroHealth = 50;
+  
+  while (heroHealth >= 0 && villainHealth >= 0) {
+      let heroDice = (Math.floor(Math.random()*20 + 1));
+      let monsterDice = (Math.floor(Math.random()*20 + 1));
 
-  	while (heroHealth > 0 && monsterHealth  > 0) {
-  		
-  		monsterDice = Math.floor(Math.random() * 20);
-      
-      heroDice = Math.floor(Math.random() * 20);
-
-  	  
-  	 if (heroDice == 1 || heroDice == 2) {
-  	 	//hero stabs for 9 point damage
-  	 	//alert new health
-		heroHealth = heroHealth - 5;
-  	 	alert(`uh-oh! ${hero} stabs self!`);
-
-  	}  	else if (heroDice >= 3 || heroDice <= 10) {
-  		alert(`${hero} misses`);
-
-  	}
-	  	else if (heroDice >= 11 || heroDice <= 18) {
-  		alert(`${hero} strikes ${monster}`);
-  		villainHealth = villainHealth - 10;
-  		
-
-  }		else if (heroDice >= 19)  {
-  		alert(`Critical hit ${hero} does double damage!`);
-  		villainHealth = villainHealth - 20;
-  		}
-
-
-if (monsterDice == 1 || monsterDice == 2) {
-  	 	//hero stabs for 9 point damage
-  	 	//alert new health
-		monsterHealth = monsterHealth - 5;
-  	 	alert(`uh-oh! ${monster} stabs self!`);
-
-  	}  	else if (monsterDice >= 3 || monsterDice <= 10) {
-  		alert(`${monster} misses`);
-
-  	}
-	  	else if (monsterDice >= 11 || monsterDice <= 18) {
-  		alert(`${monster} strikes ${hero}`);
-  		villainHealth = villainHealth - 10;
-  		
-
-  }		else if (monsterDice >= 19)  {
-  		alert(`Critical hit ${hero} does double damage!`);
-  		villainHealth = villainHealth - 20;
-  		}
-
-//Fighter's health is now zero.
- alert(`${hero} has ${heroHealth} hitpoints left, ${monster} has ${villainHealth} left.`);
-	}
-}
-	
-//Find a way to let player know who won AND who lost by name.
-
-//Add a line if they both kill each other.
-
-// Below your newly defined function, invoke `fight()`.
-fight();
-
-//Go to the browser and run the file you just created.
-
-//Ask the user if they would like to play again.
-
-//If they answer "yes" restart the game,
-
-//If they type "no", thank them for playing and end the game.
-
-//If they type anything else, ask them again.
-
-
-
-
-
-
-}
-
-
-
-  while (heroHealth >=0 && villainHealth >= 0) {
-    let heroDice = Math.floor(Math.random()*20)
-    let monsterDice = Math.floor(Math.random()*20)
-
-    if (heroDice == 1 || heroDice == 2) {
+    if (heroDice == 1 && heroDice == 2) {
       // hero stabs himself for 5 damage
       // alert the new heath
       heroHealth = heroHealth - 5;
       alert(`Critical failure! ${hero} accidentally stabs self!`);
-    } else if (heroDice >= 3 || heroDice <=10) {
+    } else if (heroDice >= 3 && heroDice <=10) {
       alert(`${hero} misses`);
-    } else if (heroDice >= 11 || heroDice <= 18) {
+    } else if (heroDice >= 11 && heroDice <= 18) {
       alert(`${hero} strikes ${monster}`);
-      villainHealth = villainHealth - 10;
+      monsterHealth = monsterHealth - 10;
     } else if (heroDice >= 19) {
       alert(`Critical hit! ${hero} does double damage!`);
-      villainHealth = villainHealth - 20;
+      monsterHealth = monsterHealth - 20;
     }
-    if (monsterDice == 1 || monsterDice == 2) {
+    if (monsterDice == 1 && monsterDice == 2) {
       // hero stabs himself for 5 damage
       // alert the new heath
-      villainHealth = villainHealth - 5;
+      monsterHealth = monsterHealth - 5;
       alert(`Critical failure! ${monster} accidentally stabs self!`);
-    } else if (monsterDice >= 3 || monsterDice <=10) {
+    } else if (monsterDice >= 3 && monsterDice <=10) {
       alert(`${monster} misses`);
-    } else if (monsterDice >= 11 || monsterDice <= 18) {
+    } else if (monsterDice >= 11 && monsterDice <= 18) {
       alert(`${monster} strikes ${hero}`);
-      villainHealth = villainHealth - 10;
+      monsterHealth = monsterHealth - 10;
     } else if (heroDice >= 19) {
       alert(`Critical hit! ${hero} does double damage!`);
-      villainHealth = villainHealth - 20;
+      monsterHealth = monsterHealth - 20;
     }
     
-    alert(`${hero} has ${heroHealth} hitpoints left, ${monster} has ${villainHealth} left.`)
+    alert(`${hero} has ${heroHealth} hitpoints left, ${monster} has ${monsterHealth} left.`);
   }
+
+  }if(monsterDice >= 3 && monsterDice <= 10) {
+heroHealth = heroHealth - 0;
+alert(
+  `${monster} has rolled ${monsterDice}, ${monster} miss ${hero}'s health is ${heroHealth}`);
+      }
+    if (heroDice >= 11 && heroDice <= 18) {
+          monsterHealth -= 10;
+    alert(`${hero} has rolled ${heroDice}, ${hero} Hits for 10 damage! ${monster}'s health is
+       ${monsterHealth}`);
+    } else if (monsterDice >= 11 && monsterDice <= 18) {
+        heroHealth -= 10;
+    alert(`${monster} has rolled ${monsterDice}, ${monster} Hits for 10 damage! ${hero} health          is ${heroHealth}`);
 }
+    if (heroDice == 19 && heroDice == 20) {
+      monsterHealth -= 20;
+    alert(`${hero} has rolled ${heroDice}, Critical HIT! ${monster}'s new health is ${monsterHealth}`);
+
+  } else if (monsterDice == 19 && monsterDice == 20) {
+      heroHealth -= 20;
+
+alert(`${monster} has rolled ${monsterDice}, Critical HIT! ${hero}'s new health is ${heroHealth}`);
+  }
+
+  if (monsterHealth <= 0 && heroHealth <= 0) {
+   alert(`Both ${hero} and ${monster} are dead!`);
+}
+  if (monsterHealth <= 0) {
+    alert(`${hero} has Won!`);
+
+  }else{
+
+alert(`${monster} has won!`);
+}
+
+fight();
 
